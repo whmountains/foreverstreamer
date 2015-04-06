e06ffc036e5526095edb4d388b3c1cefd262ab47
Maverick Mountainclimber <whmountains@icloud.com>
2015-04-06 17:21:18 -0300

    commented everything out and simply used an empty timer

--- restreamer.js
+++ restreamer.js
@@ -14,32 +14,36 @@ var url = 'http://201.238.215.3:8000';
 var header = debug('header');
 var meta   = debug('metadata');
 
-//create a watchdog timer that fires at 5 seconds
-var dog = new Watchout(5000, function() {
-  process.exit();
-});
+// //create a watchdog timer that fires at 5 seconds
+// var dog = new Watchout(5000, function() {
+//   process.exit();
+// });
+//
+// //create a transform function to watch the pcm data
+// var streamspy = through(function write(data) {
+//   this.emit('data', data);
+//   dog.reset();
+//   console.log(data[0]);
+// });
+//
+// // connect to the remote stream
+// icecast.get(url, function (res) {
+//
+//   // log the HTTP response headers
+//   header(res.headers);
+//
+//   // log any "metadata" events that happen
+//   res.on('metadata', function (metadata) {
+//     meta(icecast.parse(metadata));
+//   });
+//
+//   // Let's play the music (assuming MP3 data).
+//   // lame decodes and Speaker sends to speakers!
+//   res.pipe(new lame.Decoder())
+//      .pipe(streamspy)
+//      .pipe(new Speaker());
+// });
 
-//create a transform function to watch the pcm data
-var streamspy = through(function write(data) {
-  this.emit('data', data);
-  dog.reset();
-  console.log(data[0]);
-});
-
-// connect to the remote stream
-icecast.get(url, function (res) {
-
-  // log the HTTP response headers
-  header(res.headers);
-
-  // log any "metadata" events that happen
-  res.on('metadata', function (metadata) {
-    meta(icecast.parse(metadata));
-  });
-
-  // Let's play the music (assuming MP3 data).
-  // lame decodes and Speaker sends to speakers!
-  res.pipe(new lame.Decoder())
-     .pipe(streamspy)
-     .pipe(new Speaker());
+setInterval(100, function() {
+  //nothing
 });
