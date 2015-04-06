ed15f17e022376b3e424f66c16c71aaf11eabc91
Maverick Mountainclimber <whmountains@icloud.com>
2015-04-06 15:41:58 -0300

    added watchdog

--- restreamer.js
+++ restreamer.js
@@ -1,7 +1,11 @@
-var lame = require('lame');
-var icecast = require('icecast');
-var Speaker = require('speaker');
-var debug = require('debug');
+var lame      = require('lame');
+var icecast   = require('icecast');
+var Speaker   = require('speaker');
+var debug     = require('debug');
+var streamlog = require('through-logged');
+var through   = require('through');
+var _         = require('lodash');
+var Watchout  = require('watchout');
 
 // URL to a known Icecast stream
 var url = 'http://201.238.215.3:8000';
@@ -9,6 +13,17 @@ var url = 'http://201.238.215.3:8000';
 //set up the logging
 var header = debug('header');
 var meta   = debug('metadata');
+
+//create a watchdog timer that fires at 5 seconds
+var dog = new Watchout(5000, function() {
+  process.exit();
+});
+
+//create a transform function to watch the pcm data
+var streamspy = through(function write(data) {
+  this.emit('data', data);
+  dog.reset();
+});
 
 // connect to the remote stream
 icecast.get(url, function (res) {
@@ -24,5 +39,6 @@ icecast.get(url, function (res) {
   // Let's play the music (assuming MP3 data).
   // lame decodes and Speaker sends to speakers!
   res.pipe(new lame.Decoder())
+     .pipe(streamspy)
      .pipe(new Speaker());
 });
