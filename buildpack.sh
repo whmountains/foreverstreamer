aba471f10f966d8267290a1fa550d0b1d0a4ac07
Maverick Mountainclimber <whmountains@icloud.com>
2015-08-17 14:35:52 -0500

    added build pack.sh

---
+++ buildpack.sh
@@ -0,0 +1,44 @@
+apt-get install -y --no-install-recommends \
+	ca-certificates \
+	curl \
+	wget \
+  bzr \
+	git \
+	mercurial \
+	openssh-client \
+	subversion \
+	autoconf \
+	automake \
+	bzip2 \
+	file \
+	g++ \
+	gcc \
+	imagemagick \
+	libbz2-dev \
+	libc6-dev \
+	libcurl4-openssl-dev \
+	libevent-dev \
+	libffi-dev \
+	libglib2.0-dev \
+	libjpeg-dev \
+	liblzma-dev \
+	libmagickcore-dev \
+	libmagickwand-dev \
+	libmysqlclient-dev \
+	libncurses-dev \
+	libpq-dev \
+	libreadline-dev \
+	libsqlite3-dev \
+	libssl-dev \
+	libtool \
+	libwebp-dev \
+	libxml2-dev \
+	libxslt-dev \
+	libyaml-dev \
+	make \
+	patch \
+	xz-utils \
+	zlib1g-dev
+
+
+rm -rf /var/lib/apt/lists/*
