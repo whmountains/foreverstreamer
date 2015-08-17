31046a05951c99e162ffb8c4698b56b17cafcc76
Maverick Mountainclimber <whmountains@icloud.com>
2015-08-17 13:04:50 -0500

    more fixes to deps.sh

--- deps.sh
+++ deps.sh
@@ -4,8 +4,12 @@ set -o errexit
 set -o pipefail
 
 apt-get update
+apt-get install -y alsa-utils libasound2-dev ffmpeg curl
+
 curl -sL https://deb.nodesource.com/setup | sudo bash -
-apt-get install -y alsa-utils libasound2-dev ffmpeg nodejs
+apt-get install nodejs
+
+echo $PATH
 
 npm install forever -g
 npm install
