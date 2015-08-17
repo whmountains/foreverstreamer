02c65f5f65be0c274b775d60d19b330b3242fe02
Maverick Mountainclimber <whmountains@icloud.com>
2015-08-17 09:41:45 -0500

    streaming nuevo tiempo
    our sandbox is now successfully streaming radio nuevo tiempo

--- sandbox.js
+++ sandbox.js
@@ -3,20 +3,34 @@ var ffmpeg  = require('fluent-ffmpeg');
 var fs      = require('fs');
 var path    = require('path');
 var Speaker = require('speaker');
+var icecast   = require('icecast');
 
-// create the target stream (can be any WritableStream)
-var stream = new Speaker();
+// connect to the remote stream
+icecast.get('http://streaming.nuevotiempo.cl:8080', function (res) {
 
-// make sure you set the correct path to your video file
-var proc = ffmpeg('./t1.mp3', {presets: path.resolve('./ffmpeg-presets')})
-  // use the 'flashvideo' preset (located in /lib/presets/flashvideo.js)
-  .preset('s16le-speaker')
-  // setup event handlers
-  .on('end', function() {
-    console.log('file has been converted succesfully');
-  })
-  .on('error', function(err) {
-    console.log('an error happened: ' + err.message);
-  })
-  // save to stream
-  .pipe(stream, {end:true}); //end = true, close output stream after writing
+  // log the HTTP response headers
+  console.log(res.headers);
+
+  // log any "metadata" events that happen
+  res.on('metadata', function (metadata) {
+    console.log(icecast.parse(metadata));
+  });
+
+  // create the target stream (the system's speakers)
+  var speakers = new Speaker();
+
+  // convert the audio to pcm
+  var proc = ffmpeg(res, {presets: path.resolve('./ffmpeg-presets')})
+    // use the pcm preset
+    .preset('s16le-speaker')
+    // setup event handlers
+    .on('end', function() {
+      console.log('file has been converted succesfully');
+    })
+    .on('error', function(err) {
+      console.log('an error happened: ' + err.message);
+    })
+    // save to stream
+    .pipe(speakers, {end:true}); //end = true, close output stream after writing
+
+});
