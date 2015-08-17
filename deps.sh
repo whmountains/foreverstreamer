23db7b1a7a134fd39722cbd5d7bb0935ba105d59
Maverick Mountainclimber <whmountains@icloud.com>
2015-08-17 12:59:42 -0500

    add nodejs install do deps

--- deps.sh
+++ deps.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o pipefail
 
 apt-get update
-apt-get install -y alsa-utils libasound2-dev ffmpeg
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install -y alsa-utils libasound2-dev ffmpeg nodejs
 
 npm install forever -g
